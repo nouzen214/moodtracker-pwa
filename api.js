5f424b643d62ac219f9bfa9c298a96cf339c2fae
nouzen214 <lancealmenanza@gmail.com>
2025-11-27 15:50:14 +0800

    Add files via upload

--- api.js
+++ api.js
@@ -3,9 +3,9 @@
 // For development: Uses fallback keys below
 
 let CONFIG = {
-    FIREBASE_API_KEY: "AIzaSyCM10_89lNtUzOBIse37J2Mbc6qqPxncj0",
+    FIREBASE_API_KEY: "",
     DATABASE_URL: "https://mood-tracker-df3a2-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    GEMINI_KEY: "AIzaSyBZUc7zxOULt4gEK15qku3c1oNlRIphV6w",
+    GEMINI_KEY: "",
     GEMINI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
 };
 
